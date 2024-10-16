0e96bb0b4b6e485f13a033a5e22c6e6aec18e47d
bhismaharis <bhismaharis11@gmail.com>
2024-10-16 13:14:56 +0700

    [01.2] rename file manufacture to manufactures

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 
 const carsRouter = require("./cars.routes");
-const manufactureRouter = require("./manufacture.routes");
+const manufacturesRouter = require("./manufactures.routes");
 const typesRouter = require("./types.routes");
 
 const router = express.Router();
 
 router.use("/cars", carsRouter );
-router.use("/manufacture", manufactureRouter );
+router.use("/manufactures", manufacturesRouter );
 router.use("/types", typesRouter );
 
 module.exports = router;
